042247acb8369758671dcd535cf2e81c55d5c300
ChinniSaiteja <chinnisaiteja8688@gmail.com>
2024-05-01 18:23:27 +0530

    xn

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
-
+import { BrowserRouter as Router,Routes,Route } from "react-router-dom"
+import Header from "./components/Header"
+import CoinDetails from "./components/CoinDetails"
+import Coins from "./components/Coins"
+import Exchanges from "./components/Exchanges"
+import Footer from "./components/Footer"
+import Home from "./components/Home"
 
 function App() {
-  return(<div>Hii</div>)
+  return(
+    <Router>
+      <Header/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/coins" element={<Coins/>}/>
+        <Route path="/exchanges" element={<Exchanges/>}/>
+        <Route path="/coin/:id" element={<CoinDetails/>}/>
+      </Routes>
+    </Router>
+  )
 }
 
 export default App
